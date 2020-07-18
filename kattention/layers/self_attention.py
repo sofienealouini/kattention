db6d3d02e10e308b3a73722ed82ce68a914e7110
Sofiene Alouini <sofiene.alouini@gmail.com>
2020-07-18 12:37:25 +0200

    FEAT: Add scaling by the inverse of embedding_size to context weights in SelfAttention

--- kattention/layers/self_attention.py
+++ kattention/layers/self_attention.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.layers import Layer, Dense, Dot, Softmax
+from tensorflow.python.keras.layers import Lambda
 
 
 class SelfAttention(Layer):
@@ -9,6 +10,7 @@ class SelfAttention(Layer):
         self.dense_k = None
         self.dense_v = None
         self.dot_context = Dot(axes=(2, 2))
+        self.scale = None
         self.normalize = Softmax()
         self.reweight_v = Dot(axes=(2, 1))
 
@@ -17,11 +19,14 @@ class SelfAttention(Layer):
         self.dense_q = Dense(units=embedding_size, activation='linear')
         self.dense_k = Dense(units=embedding_size, activation='linear')
         self.dense_v = Dense(units=embedding_size, activation='linear')
+        scaling_factor = 1 / embedding_size
+        self.scale = Lambda(lambda x: x * scaling_factor)
 
     def call(self, inputs, **kwargs):
         queries = self.dense_q(inputs)
         keys = self.dense_k(inputs)
         values = self.dense_v(inputs)
         context_weights = self.dot_context([queries, keys])
-        normalized_context_weights = self.normalize(context_weights)
+        scaled_context_weights = self.scale(context_weights)
+        normalized_context_weights = self.normalize(scaled_context_weights)
         return self.reweight_v([normalized_context_weights, values])
